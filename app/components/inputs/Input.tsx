8ae480650cbfb8da7faf8a711782034e36e7d0d0
Mostafa Valadi <m.valadi2002@gmail.com>
2024-03-19 00:45:39 +0330

    Listing creation step 5(Description and Price, Listing creation POST route)

--- app/components/inputs/Input.tsx
+++ app/components/inputs/Input.tsx
@@ -96,7 +96,7 @@ const Input: React.FC<InputProps> = ({
             Please enter your name between 3 character and maximum 10 character
           </label>
         </>
-      ) : (
+      ) : id === "password" ? (
         <>
           <input
             id={id}
@@ -129,6 +129,30 @@ const Input: React.FC<InputProps> = ({
             className="underline cursor-pointer absolute top-5 right-5"
             onClick={() => setIsShow((value) => !value)}
           >{`${isShow ? "Hide" : "Show"}`}</label>
+        </>
+      ) : (
+        <>
+          <input
+            id={id}
+            disabled={disabled}
+            {...register(id, { required })}
+            placeholder=" "
+            type={`${isShow ? "text" : type}`}
+            className={`peer w-full p-4 pt-6 font-light bg-white border-2 rounded-md outline-none transition disabled:opacity-70 disabled:cursor-not-allowed ${
+              formatPrice ? "pl-9" : "pl-4"
+            } ${errors[id] ? "border-rose-500" : "border-neutral-300"} ${
+              errors[id] ? "focus:border-rose-500" : "focus:border-black"
+            }`}
+          />
+          <label
+            className={`absolute text-md duration-150 transform -translate-y-3 top-5 z-10 origin-[0] ${
+              formatPrice ? "left-9" : "left-4"
+            } peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-4 ${
+              errors[id] ? "text-rose-500" : "text-zinc-400"
+            }`}
+          >
+            {label}
+          </label>
         </>
       )}
     </div>
