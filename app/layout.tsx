8643838b8ebe999af6aca414aaedca0f24a318cb
Mostafa Valadi <m.valadi2002@gmail.com>
2024-03-20 21:42:13 +0330

    Fetching listings with server components(Listing card component, direct server action)

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,14 +51,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        {/* <ClientOnly> */}
-        <ToasterProvider />
-        <RentModal />
-        <LoginModal />
-        <RegisterModal />
-        <Navbar currentUser={currentUser} />
-        {/* </ClientOnly> */}
-        {children}
+        <ClientOnly>
+          <ToasterProvider />
+          <RentModal />
+          <LoginModal />
+          <RegisterModal />
+          <Navbar currentUser={currentUser} />
+        </ClientOnly>
+        <div className="pb-20 pt-28">{children}</div>
       </body>
     </html>
   );
