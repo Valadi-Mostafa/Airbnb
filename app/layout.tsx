a4dfc85ec60d3ee8ccf82b3f909afd33f9849e80
Mostafa Valadi <m.valadi2002@gmail.com>
2024-04-13 15:16:50 +0330

    Filters modal(Assigning various filters, add advanced querying logic to getListings, add loading and error pages)

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import { Metadata } from "next";
 import { Nunito } from "next/font/google";
+import dynamic from "next/dynamic";
 
 import "./globals.css";
 
-import dynamic from "next/dynamic";
-
 import ClientOnly from "./components/ClientOnly";
-// import Navbar from "./components/navbar/Navbar";
-// import RegisterModal from "./components/modals/RegisterModal";
-// import LoginModal from "./components/modals/LoginModal";
-// import RentModal from "./components/modals/RentModal";
-// import ToasterProvider from "./providers/ToasterProvider";
-
 const Navbar = dynamic(() => import("./components/navbar/Navbar"), {
   ssr: false,
 });
@@ -30,6 +23,10 @@ const RentModal = dynamic(() => import("./components/modals/RentModal"), {
 const ToasterProvider = dynamic(() => import("./providers/ToasterProvider"), {
   ssr: false,
 });
+const SearchModal = dynamic(() => import("./components/modals/SearchModal"), {
+  ssr: false,
+});
+
 import getCurrentUser from "./actions/getCurrentUser";
 
 const metadata: Metadata = {
@@ -53,6 +50,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <SearchModal />
           <RentModal />
           <LoginModal />
           <RegisterModal />
